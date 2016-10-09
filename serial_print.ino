dd4c86b7e49754bd441d0469acd56f917521bbad
ianamsyi <ianamsyi19@gmail.com>
2016-10-09 09:36:50 +0700

    Add files via upload

---
+++ serial_print.ino
@@ -0,0 +1,12 @@
+void setup() { Serial.begin(115200);
+}
+
+void loop() {
+  Serial.print(0x00);
+  Serial.print(0x01);
+  Serial.print(0x02);
+  Serial.print(0x03); 
+  Serial.print(0x04);
+  delay(2000);
+}
+
